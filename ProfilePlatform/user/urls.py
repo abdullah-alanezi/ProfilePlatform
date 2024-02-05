1b8d2f69947a4bc0ed2abfbabeb5ff91171732e9
abdullah <abdullah.alanezi.23@gmail.com>
2024-02-05 15:32:17 +0300

    adding user login and user logout operations

--- ProfilePlatform/user/urls.py
+++ ProfilePlatform/user/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-
+from .import views
 
 app_name = 'user'
 
 urlpatterns =[
-    
+    path('register/',views.register_view, name='register_view'),
+    path('login/',views.log_in_view,name ='log_in_view'),
+    path('logout/',views.logout_view,name='logout_view')
 ]
--- ProfilePlatform/user/views.py
+++ ProfilePlatform/user/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http import HttpRequest
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login, logout
 # Create your views here.
+
+
+def register_view(request:HttpRequest):
+    if request.method == 'POST':
+        user =User.objects.create_user(username=request.POST['username'],email=request.POST['email'],password=request.POST['password'])
+        user.save()
+        return redirect('main:profile_view')
+    return render(request,'user/register.html')
+
+
+
+
+def log_in_view(request:HttpRequest):
+    msg=None
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            user = authenticate(username =request.POST['username'],password = request.POST['password'])
+
+            if user:
+                login(request,user)
+                return redirect('main:profile_view')
+            else:
+                msg="please inter correct username and password"
+
+    return render(request,'user/login.html',{'msg':msg})
+
+
+def logout_view(request:HttpRequest):
+
+    if request.user.is_authenticated:
+        logout(request)
+        return redirect('user:log_in_view')
